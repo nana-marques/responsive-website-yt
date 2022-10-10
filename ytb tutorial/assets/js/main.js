fa6580c7bb9fe3c8df71b7bf83144d1ae50d34ef
Ana Marques <90800345+nana-marques@users.noreply.github.com>
2022-10-10 15:50:29 -0300

    Add files via upload

--- ytb tutorial/assets/js/main.js
+++ ytb tutorial/assets/js/main.js
@@ -91,3 +91,19 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+
+// portfolio
+
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+});
